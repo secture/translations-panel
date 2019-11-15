2713758e64814f02b081a58d3c4cce83ff354583
danielgbullido <daniel.gbullido@gmail.com>
2019-11-15 10:29:05 +0100

    add snackbar

--- src/services/categories.ts
+++ src/services/categories.ts
@@ -13,11 +13,15 @@ export const getAllCategories = (): ThunkAction<Promise<any>, {}, {}, AnyAction>
             dispatch(setAllCategories(categories.data));
             dispatch(setStatus({
                 type: 'success',
-                message: 'updated',
+                message: 'All categories have been obtained successfully ',
                 show: true
             }));
         } catch (error) {
-            console.log(error);
+            dispatch(setStatus({
+                type: 'error',
+                message: 'Error getting categories',
+                show: true
+            }));
         }
     }
 };
@@ -28,8 +32,17 @@ export const deleteCategoryById = (category: CategoryState): ThunkAction<Promise
         try {
             categories = await httpClient.delete(`${process.env.REACT_APP_API_URL}/v1/categories/${category.id}`);
             dispatch(getAllCategories());
+            dispatch(setStatus({
+                type: 'success',
+                message: 'Deleted category successfully',
+                show: true
+            }));
         } catch (error) {
-            console.log(error);
+            dispatch(setStatus({
+                type: 'error',
+                message: 'Error deleting category',
+                show: true
+            }));
         }
         return categories;
     }
@@ -41,8 +54,17 @@ export const editCategoryById = (category: CategoryState): ThunkAction<Promise<a
         try {
             categories = await httpClient.put(`${process.env.REACT_APP_API_URL}/v1/categories/${category.id}`, category);
             dispatch(getAllCategories());
+            dispatch(setStatus({
+                type: 'success',
+                message: 'Edited category successfully',
+                show: true
+            }));
         } catch (error) {
-            console.log(error);
+            dispatch(setStatus({
+                type: 'error',
+                message: 'Error editing category',
+                show: true
+            }));
         }
         return categories;
     }
@@ -53,8 +75,17 @@ export const addCategory = (category: CategoryState): ThunkAction<Promise<any>, 
         try {
             categories = await httpClient.post(process.env.REACT_APP_API_URL + '/v1/locales', category);
             dispatch(getAllCategories());
+            dispatch(setStatus({
+                type: 'success',
+                message: 'Added category successfully',
+                show: true
+            }));
         } catch (error) {
-            console.log(error);
+            dispatch(setStatus({
+                type: 'error',
+                message: 'Error adding category',
+                show: true
+            }));
         }
         return categories;
     }
