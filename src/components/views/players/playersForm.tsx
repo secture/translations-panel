8faf787f7b8cc86e14f0e98fd994d4a0364f35eb
sergiorodriguez <sergio.gundin.rodriguez@gmail.com>
2019-11-21 12:48:09 +0100

    - merge error duplicated code

--- src/components/views/players/playersForm.tsx
+++ src/components/views/players/playersForm.tsx
@@ -1,5 +1,3 @@
-import {formStyles} from 'styles/form'
-import Paper from "@material-ui/core/Paper";
 import React, {useState} from "react";
 import {PlayerState} from "store/players/types";
 import {LanguageState} from "store/languages/types";
